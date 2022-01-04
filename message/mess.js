979d91b52dcd45dcc3a8e5374e7a5a4dab7f577a
Awan-Btz <92975544+Awan-Btz@users.noreply.github.com>
2022-01-04 08:16:52 +0700

    Update mess.js

--- message/mess.js
+++ message/mess.js
@@ -4,13 +4,13 @@ module.exports.mess = {
 			wrongFormat: 'Format salah, coba liat lagi di menu',
 			error: {
 				api: 'APINYA HABIS BRO ISI ULANG DULU',
-				stick: 'ITU BUKAN STIKER TOLOL',
-				Iv: 'JANGAN NGASIH LINK GAJE TOD'
+				stick: 'ITU BUKAN STIKER',
+				Iv: 'JANGAN NGASIH link'
 			},
 			only: {
 				group: 'KHUSUS GRUP BRO',
 				admin: 'KHUSUS ADMIN GRUP BRO',
-				premium: 'Sadar Bro Lu Bukan User Premium\nFitur Ini Khusus User Premium Awokawoakwo',
+				premium: 'Bro Lu Bukan User Premium\nFitur Ini Khusus User Premium',
 				owner: 'KHUSUS OWNER BRO',
 				Badmin: 'BOT HARUS JADI ADMIN BRO',
 			}
